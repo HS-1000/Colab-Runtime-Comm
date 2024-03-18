e16cbf0a3deee964ec901fd9eaccc796ee432597
HS-1000 <68994620+HS-1000@users.noreply.github.com>
2024-03-19 02:19:49 +0900

    기본적인 기능 구상

---
+++ comm.py
@@ -0,0 +1,15 @@
+import pickle as pk
+
+"""
+호출해서 사용할 session클래스: 이 클래스를 사용해서 서버와 pickle파일 통신
+"""
+
+"""
+session클래스에서 사용할 보조적인 함수들 
+가능하면 클래서 함수로 작성하기 (write, read, delete, ...)
+"""
+class Session:
+    pass
+
+
+
